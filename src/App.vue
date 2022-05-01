0bbb8ffb5a0fcbe01ed7ee33e199c712a2119ca4
linh <571187984@qq.com>
2022-05-01 17:20:28 +0800

    this is qiankun base

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/vue">Sub</router-link>
     </nav>
     <router-view/>
+    <div id="vueApp" />
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { registerMicroApps, start } from 'qiankun'
 
 Vue.config.productionTip = false
 
@@ -10,3 +11,28 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+registerMicroApps([{
+  name: 'vue app',
+  entry: { scripts: ['//localhost:8888'] },
+  container: '#vueApp',
+  activeRule: '/vue'
+}],
+{
+  beforeLoad: [
+    app => {
+      console.log('[LifeCycle] before load %c%s', 'color: green;', app.name)
+    }
+  ],
+  beforeMount: [
+    app => {
+      console.log('[LifeCycle] before mount %c%s', 'color: green;', app.name)
+    }
+  ],
+  afterUnmount: [
+    app => {
+      console.log('[LifeCycle] after unmount %c%s', 'color: green;', app.name)
+    }
+  ]
+})
+start()
